c7e31d8493e321eeb536f3d0219f0d425cda57ba
hisoka <1399952343@qq.com>
2022-08-14 13:26:14 +0800

    初始化

--- src/CommentApp.js
+++ src/CommentApp.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import CommentInput from "./commentInput";
 import CommentList from "./commentList";
-
+import "./index.css"
 class CommentApp extends Component {
     render() {
         return (
-            <div>input
+            <div className="wrapper">
                 <CommentInput></CommentInput>
                 <CommentList></CommentList>
             </div>
--- src/commentInput.js
+++ src/commentInput.js
@@ -3,7 +3,21 @@ import React, { Component } from "react";
 class CommentInput extends Component {
     render() {
         return (
-            <div>input</div>
+            <div className="wrapperinput">
+                <div className="winput">
+                    <div className="nameinput">
+                        <span>姓名：</span>
+                        <input className="ninput"></input>
+                    </div>
+                    <div className="contentinput">
+                        <span>评论：</span>
+                        <textarea className="cinput"></textarea>
+                    </div>
+                    <div className="buttoninput">
+                        <button className="binput">发布</button>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
